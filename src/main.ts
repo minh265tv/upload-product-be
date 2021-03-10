94e58fab064d44716197c0985d883dc4e0f4a43d
Nguyen Quang Minh <minh.tonn@gmail.com>
2021-03-10 14:27:22 +0700

    commit code

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log(process.env)
   const port = process.env.PORT || 8080;
   await app.listen(port);
 }
