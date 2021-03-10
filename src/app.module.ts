eeff2d51961c5603e5c52a7b36f1ee76c11e5c27
Nguyen Quang Minh <minh.tonn@gmail.com>
2021-03-10 13:45:26 +0700

    commit code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/product.module';
 
+const uriMongodb: string = process.env.MONGO_DB_URI || 'mongodb://localhost:27017/shopify';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/shopify'), ProductsModule]
+  imports: [MongooseModule.forRoot(uriMongodb), ProductsModule]
 })
 export class AppModule {}
